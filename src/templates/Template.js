22c32baeeb2fa7eaa0c207246418a7617c9e3342
villafrade <cavillafrades@gmail.com>
2022-03-29 07:33:58 -0500

    mis redes

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -27,13 +27,13 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https://www.facebook.com/carlosalberto.villafradespinilla.5">
             <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/villafrade">
             <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
+          <a href="https://www.instagram.com/carlos_villafradess">
             <img src="${instagram}" />
           </a>
         </div>
